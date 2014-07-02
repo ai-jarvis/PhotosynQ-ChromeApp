fefc119d2b14f8d9f7fbabebb85d08d0fb71f1a6
SeBassTian23 <sebastian.kuhlgert@gmail.com>
2014-07-01 22:11:35 -0400

    Using html5 location service now instead of freegeoip.org

--- location.js
+++ location.js
@@ -1,29 +1,19 @@
 function GetLocation(){
-	/*
-	Using the free service freegeoip.net
-	to get the location until the location service for the chrome extension
-	leaves the developer version.
-	
-	freegeoip.net/{format}/{ip_or_hostname}{format}/{ip_or_hostname}
-	{format} csv, xml or json
-	*/
 	if(navigator.onLine){
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "https://freegeoip.net/json/", true);
-		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		xhr.send();
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4){
-				try {
-					_geolocation = JSON.parse(xhr.responseText);
-				} catch (e) {
-					WriteMessage('Invalid location information.','danger');
-					return;
-				}
+
+		navigator.geolocation.getCurrentPosition(
+			function(pos){
+				console.log(pos.coords);
+				_geolocation = {}
+				_geolocation.latitude = pos.coords.latitude
+				_geolocation.longitude = pos.coords.longitude
+				_geolocation.accuracy = pos.coords.accuracy
+				
 				SaveToStorage('geolocation',_geolocation,function(){});
+				
 				WriteMessage('Geo Location updated','info');
-				$('#CurrentLocationDisplay,#CurrentLocationDisplayImgCoords').html('<small class="text-muted"><i class="fa fa-location-arrow"></i> '+_geolocation.city+', '+_geolocation.region_code+' - '+_geolocation.country_code+' ('+_geolocation.latitude+', '+_geolocation.longitude+')</small>');
-				$('#CurrentLocationIndicator').toggleClass('text-muted fa-inverse').attr('title', _geolocation.city+', '+_geolocation.region_code+' - '+_geolocation.country_code+' ('+_geolocation.latitude+', '+_geolocation.longitude+')');
+				$('#CurrentLocationDisplay,#CurrentLocationDisplayImgCoords').html('<small class="text-muted"><i class="fa fa-location-arrow"></i> '+_geolocation.latitude+', '+_geolocation.longitude+'</small>');
+				$('#CurrentLocationIndicator').toggleClass('text-muted fa-inverse').attr('title', _geolocation.latitude+', '+_geolocation.longitude);
 				var url = 'http://maps.googleapis.com/maps/api/staticmap?center='+_geolocation.latitude+','+_geolocation.longitude+'&zoom=15&size=300x175&maptype=roadmap&markers=color:red|'+_geolocation.latitude+','+_geolocation.longitude+'&sensor=false'
 				if(_media[url] !== undefined){
 					$('#CurrentLocationDisplayImg').html('<img src="'+_media[url]+'">');
@@ -35,15 +25,22 @@ function GetLocation(){
 							if(xhrMap.response !== null && xhrMap.response !== undefined){
 								$('#CurrentLocationDisplayImg').html('<img src="'+window.URL.createObjectURL(xhrMap.response)+'">');
 								SaveImgToLocalStorage(url,window.URL.createObjectURL(xhrMap.response));
-								console.log('save')
 							}
 						}
 					}
 					xhrMap.open('GET', url, true);
 					xhrMap.send();
-				}
+				}				
+			},
+			function(error){
+				WriteMessage(err.message,'danger');
+			},
+			{
+			  enableHighAccuracy: true,
+			  timeout: 5000,
+			  maximumAge: 0
 			}
-		}
+		);
 	}
 	else{
 		chrome.storage.local.get('geolocation', function(result){
@@ -56,8 +53,10 @@ function GetLocation(){
 					return;
 				}
 				WriteMessage('Geo Location from cache','info');
-				$('#CurrentLocationDisplay').html('<small><i class="fa fa-location-arrow"></i> '+_geolocation.city+', '+_geolocation.region_code+' - '+_geolocation.country_code+' ('+_geolocation.latitude+', '+_geolocation.longitude+')</small>');
-				$('#CurrentLocationIndicator').toggleClass('text-muted fa-inverse').attr('title', _geolocation.city+', '+_geolocation.region_code+' - '+_geolocation.country_code+' ('+_geolocation.latitude+', '+_geolocation.longitude+') - Cached');
+				$('#CurrentLocationDisplay,#CurrentLocationDisplayImgCoords').html('<small class="text-muted"><i class="fa fa-location-arrow"></i> '+_geolocation.latitude+', '+_geolocation.longitude+'</small>');
+				$('#CurrentLocationIndicator').toggleClass('text-muted fa-inverse').attr('title', _geolocation.latitude+', '+_geolocation.longitude);
+				if(_media[url] !== undefined){
+					$('#CurrentLocationDisplayImg').html('<img src="'+_media[url]+'">');
 			}
 		});
 	
