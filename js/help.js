c3628674d3e2187152ff325f7ecdffb823d20f09
SeBassTian23 <sebastian.kuhlgert@gmail.com>
2014-07-25 15:35:32 -0400

    Updated help documentation, simple search added

--- js/help.js
+++ js/help.js
@@ -9,4 +9,22 @@ onload = function() {
 	});
 	var version = chrome.runtime.getManifest().version;
 	$('#AppVersion').text('Version: '+version);
+	
+	// Filter Parameter
+	// =====================================================================
+	jQuery.expr[':'].contains = function(a, i, m) { 
+		return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0; 
+	};
+
+	$('#SearchHelpContent').on('keyup', function(){
+		if($(this).val() != "" && $(this).val().length > 1){
+			$('div [id^="Help"], hr').hide();
+			$('div [id^="Help"]:contains("'+$(this).val()+'")').show();
+		}
+		else{
+			$('div [id^="Help"],hr').show();
+		}	
+		return false;
+	});
+	
 }
