cbecd0c4c310dc6ed277f6803e3461053b9dbe77
SeBassTian23 <sebastian.kuhlgert@gmail.com>
2014-07-25 15:32:35 -0400

    Added/changed tooltips, changed update location

--- location.js
+++ location.js
@@ -10,7 +10,7 @@ function GetLocation(){
 			
 				WriteMessage('Geo location updated','info');
 				$('#CurrentLocationDisplay').html('<small class="text-muted"><i class="fa fa-location-arrow"></i> '+_geolocation.latitude+', '+_geolocation.longitude+'</small>');
-				$('#CurrentLocationIndicator').removeClass('text-muted').addClass('fa-inverse').attr('title', _geolocation.latitude+', '+_geolocation.longitude);
+				$('#CurrentLocationIndicator').removeClass('text-muted').addClass('fa-inverse').attr('title', 'Your location: '+_geolocation.latitude+', '+_geolocation.longitude);
 				var url = 'http://maps.googleapis.com/maps/api/staticmap?center='+_geolocation.latitude+','+_geolocation.longitude+'&zoom=15&size=300x175&maptype=roadmap&markers=color:red|'+_geolocation.latitude+','+_geolocation.longitude+'&sensor=false'
 				DatabaseGetImage('map',url,function(img){
 					$('#CurrentLocationDisplayImg').html(img.img+'<small class="text-muted"><i class="fa fa-location-arrow"></i> '+_geolocation.latitude+', '+_geolocation.longitude+' &plusmn; '+pos.coords.accuracy+' m</small>');
